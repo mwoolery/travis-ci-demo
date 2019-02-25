9d3e96e2fb62b29d1d4e11aa6a0e1a6a65736461
mwoolery <mwoolery@nwmissouri.edu>
2019-02-24 20:03:21 -0600

    Travis CI demo setup

---
+++ test.js
@@ -0,0 +1,30 @@
+var assert = require('assert');
+
+var calc = require('./math.js');
+
+
+// Tests are hierarchical. Here we define a test suite for our calculator.
+describe('Math Unit Tests', function() {
+	// And then we describe our testcases.
+	it('returns 2+2=4', function(done) {
+		assert.equal(calc.add(2, 2), 4);
+		// Invoke done when the test is complete.
+		done();
+	});
+
+	it('returns 2*2=4', function(done) {
+		assert.equal(calc.multiply(2, 2), 4);
+		// Invoke done when the test is complete.
+		done();
+    });
+    it('returns 2/2=1', function(done) {
+		assert.equal(calc.divide(2, 2), 1);
+		// Invoke done when the test is complete.
+		done();
+    });
+    it('returns 2-2=0', function(done) {
+		assert.equal(calc.subtract(2, 2), 0);
+		// Invoke done when the test is complete.
+		done();
+	});
+});
